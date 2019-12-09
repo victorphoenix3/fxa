d2bd4d13f3d200fa558f4e97fe0d2cc4c0331ab8
Marty Ballard <marty331@gmail.com>
2019-12-08 23:09:39 -0600

    Added Survey in iFrame - Survey added via iframe - Redirects to download page once complete

--- packages/fxa-payments-server/src/routes/Product/SubscriptionRedirect/index.tsx
+++ packages/fxa-payments-server/src/routes/Product/SubscriptionRedirect/index.tsx
@@ -23,27 +23,45 @@ export const SubscriptionRedirect = ({ plan }: SubscriptionRedirectProps) => {
   const redirectUrl =
     downloadURL || productRedirectURLs[product_id] || defaultProductRedirectURL;
 
+  // useEffect(() => {
+  //   window.addEventListener('message', handleIframeTask);
+  // });
+
+  // const handleIframeTask = (e: any) => {
+  //   if (e.data === 'submitted survey') {
+  //     navigateToUrl(redirectUrl);
+  //   }
+  // };
   useEffect(() => {
-    navigateToUrl(redirectUrl);
-  }, [navigateToUrl, redirectUrl]);
+    const handleIframeTask = (e: any) => {
+      if (e.data === 'submitted survey') {
+        navigateToUrl(redirectUrl);
+      }
+    };
+    window.addEventListener('message', handleIframeTask);
+    return () => window.removeEventListener('message', handleIframeTask);
+  }, [redirectUrl]);
 
   return (
     <div className="product-payment" data-testid="subscription-redirect">
       <div className="subscription-ready">
-        <h2>Your subscription is ready</h2>
-        <img
-          alt={product_name}
-          src={webIconURL || fpnImage}
-          width="96"
-          height="96"
-        />
-        <p>
-          Hang on for a moment while we send you to the{' '}
-          <span className="plan-name">{product_name}</span> download page.
-        </p>
-        <a href={redirectUrl}>
-          Click here if you're not automatically redirected
-        </a>
+        <div className="subscription-message">
+          <h2>Your subscription is ready</h2>
+        </div>
+        <div className="form-break-top break-buffer"></div>
+        <div className="exp-message">
+          Please take a moment to tell us about your experience.
+        </div>
+        <div className="survey-frame">
+          <iframe
+            sandbox="allow-scripts allow-forms"
+            src="http://www.surveygizmo.com/s3/5294819/VPN-Subscription?__no_style=true"
+          ></iframe>
+        </div>
+        <div className="form-break-bottom break-buffer"></div>
+        <div>
+          <a href={redirectUrl}>No thanks, just take me to my product.</a>
+        </div>
       </div>
     </div>
   );
